7c1df7a005ae3dbf770797de83540612ca10a5f2
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-08-30 21:23:32 +0600

    Add sign up page and fuctionality

--- quixell/quixell/urls.py
+++ quixell/quixell/urls.py
@@ -3,11 +3,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from core.views import index, contact
-
 urlpatterns = [
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
+    path('', include('core.urls')),
     path('items/', include('items.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
