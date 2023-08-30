a147aa803e1ad46377b4489392e4a16277cccbd4
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-08-30 12:22:10 +0600

    Add Category and Item models

--- quixell/items/models.py
+++ quixell/items/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+      verbose_name = 'Category'
+      verbose_name_plural = 'Categories'
+      ordering = ('name', )
+
+
+
+class Item:
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField()
+    created_by = models.ForeignKey(User, related_name='items',  on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+       return self.name
