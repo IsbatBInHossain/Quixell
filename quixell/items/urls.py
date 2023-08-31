79c70942475e4a87d9f23c902244bcc3a68475ab
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-08-31 20:49:19 +0600

    Delete item functionality

--- quixell/items/urls.py
+++ quixell/items/urls.py
@@ -6,5 +6,6 @@ app_name = 'items'
 
 urlpatterns = [
     path('<int:pk>/', views.detail , name='detail'),
+    path('<int:pk>/delete/', views.delete , name='delete'),
     path('new/', views.new, name='new')
 ]
